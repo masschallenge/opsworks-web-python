ba69204e72a0b9d5e5ba6d628c3f1793c155cb2f
Shankar Ambady <shanbady@gmail.com>
2020-03-11 18:10:46 -0400

    adding sudo

--- opsworks_deploy_python/recipes/install_pip36.rb
+++ opsworks_deploy_python/recipes/install_pip36.rb
@@ -11,11 +11,11 @@ node[:deploy].each do |application, deploy|
         user "root"
         environment node['deploy']['mc']['environment']
         code <<-EOH
-           add-apt-repository -y ppa:deadsnakes/ppa
-           apt-get update
-           apt-get install -y python3.6
-           update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-           wget https://bootstrap.pypa.io/get-pip.py -O - | python3
+           sudo add-apt-repository -y ppa:deadsnakes/ppa
+           sudo apt-get update
+           sudo apt-get install -y python3.6
+           sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+           wget https://bootstrap.pypa.io/get-pip.py -O - | sudo python3
            curl https://bootstrap.pypa.io/ez_setup.py -o - | sudo python3.6 && sudo python3.6 -m easy_install pip
         EOH
     end
