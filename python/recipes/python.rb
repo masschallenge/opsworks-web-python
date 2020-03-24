32604fb145630f8db13381c9081e6bfa2c0c3224
Shankar Ambady <shanbady@gmail.com>
2020-03-24 13:31:11 -0400

    moving commands around

--- python/recipes/python.rb
+++ python/recipes/python.rb
@@ -10,7 +10,11 @@ end
 
 package 'python3.6' do
     options '--force-yes'
-    action :install
+    command 'sudo add-apt-repository -y ppa:deadsnakes/ppa'
+	command 'sudo apt-get update & sudo apt-get install -y python3.6'
+	command 'sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1'
+	command 'wget https://bootstrap.pypa.io/get-pip.py -O - | sudo python3'
+	command 'sudo curl https://bootstrap.pypa.io/ez_setup.py -o - | sudo python3.6 && sudo python3.6 -m easy_install pip'
 end
 
 package 'python3.6-dev' do
